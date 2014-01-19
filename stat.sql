ea0e9c496a987f699260dcfde0e829fa6ad46c34
YunL <962489669@qq.com>
2014-01-19 21:41:29 +0800

    注册/收益曲线增加渠道
    收益暂时不增加，该功能后续考虑；注册功能在文档上已进行channel的增加，关于注册和收益的历史曲线也增加的查询历史曲线的脚本，暂不使用新表进行历史数据存储

--- stat.sql
+++ stat.sql
@@ -1,29 +1,32 @@
 ##每十分钟统计一次收入并插入收入统计表中
-INSERT INTO `stat_money`(game_id,channel,amount,postdate,posttime)
-SELECT gameid,channel,IFNULL(SUM(buyprice)/100,0),CURDATE(),CURTIME()
-FROM `charge` 
-WHERE buytime>=CURDATE() AND buyresult=3
-GROUP BY gameid,channel;
+INSERT INTO `stat_money`(game_id,amount,postdate,posttime)
+SELECT 100,
+       IF(0=(SELECT COUNT(1) FROM `charge` WHERE buytime>=CURDATE() AND buyresult=3),
+          0,
+          (SELECT IFNULL(SUM(buyprice)/100,0) FROM `charge` WHERE buytime>=CURDATE() AND buyresult=3 GROUP BY gameid)),
+       CURDATE(),
+       CURTIME()
 
 ##################################
-##查询某款游戏单日各渠道收入曲线
+##查询某款游戏收入曲线
 ##设入参i_gameid 是游戏标志
 ##设入参i_postdate是查询曲线日期
 ##################################
-SELECT channel,amount,posttime
+SELECT amount,posttime
 FROM `stat_money`
 WHERE game_id=i_gameid AND postdate=i_postdate ;
 
+
 ##################################
-##查询游戏单日收入曲线
+##查询历史收入曲线（以天为单位）
 ##设入参i_gameid 是游戏标志
-##设入参i_postdate是查询曲线日期
+##设入参i_start是开始查询曲线日期
+##设入参i_end是结束查询曲线日期
 ##################################
-SELECT SUM(amount),posttime
+SELECT game_id,postdate,MAX(amount) AS amount
 FROM `stat_money`
-WHERE game_id=i_gameid AND postdate=i_postdate
-GROUP BY posttime
-ORDER BY posttime;
+WHERE game_id=i_gameid AND postdate BETWEEN i_start AND i_end
+GROUP BY game_id,postdate
 
 
 ##每十分钟统计一次注册人数并插入用户统计表中
@@ -34,25 +37,41 @@ WHERE `registertime`>=CURDATE()
 GROUP BY channel;
 
 ##################################
-##查询某款游戏单日各渠道注册人数
+##查询单日各渠道注册人数
 ##设入参i_postdate是查询曲线日期
 ##设入参i_channel是渠道号
 ##################################
+
+##No.1  一个界面中显示一个渠道
+SELECT register_amount,posttime
+FROM `stat_users`
+WHERE postdate=i_postdate AND channel=i_channel
+ORDER BY posttime;
+##No.1  一个界面中显示多个渠道
 SELECT channel,register_amount,posttime
 FROM `stat_users`
-WHERE game_id=i_gameid AND postdate=i_postdate;
+WHERE postdate=i_postdate 
+ORDER BY channel,posttime;
 
 ##################################
-##查询某款游戏单日注册人数
+##查询单日注册总人数
 ##设入参i_postdate是查询曲线日期
-##设入参i_channel是渠道号
 ##################################
 SELECT SUM(register_amount),posttime
 FROM `stat_users`
-WHERE game_id=i_gameid AND postdate=i_postdate
+WHERE postdate=i_postdate
 GROUP BY posttime
 ORDER BY posttime;
 
 
-PS:查询需求要问下文著兄，然后，改结构和补数据，最好是两个人都在的时候改，补数据补多久的？
+##################################
+##查询历史注册人数（以天为单位）
+##设入参i_start是开始查询曲线日期
+##设入参i_end是结束查询曲线日期
+##################################
+SELECT postdate,channel,MAX(register_amount) AS amount
+FROM stat_users
+WHERE postdate BETWEEN i_start AND i_end
+GROUP BY postdate,channel
+
 
