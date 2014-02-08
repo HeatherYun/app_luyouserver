796c90aaac927a5c7acd080e97ad19ccd2b2e50d
YunL <962489669@qq.com>
2014-02-08 12:04:08 +0800

    周排名优化
    1、新增表存储当前周用户最高积分
    2、当前周用户排名信息语句更新

--- ranks.sql
+++ ranks.sql
@@ -31,66 +31,49 @@ FROM ranks
 WHERE weeks = IF(DAYOFWEEK(CURDATE())=1,WEEK(CURDATE())-2,WEEK(CURDATE())-1) AND uid = i_uid;
 
 
+
+
+
+##查询用户本周最高积分，有可能返回空行
+##设i_uid为用户标志入参
+SELECT total_score
+FROM `cur_week_score` 
+WHERE uid=i_uid
+
+
+##如果是本周最高积分就插入
+##UID为主键，如果该用户本周最高积分已经存在，则直接替换，
+##所以必须程序先判断修改后的积分是最高积分
+REPLACE INTO cur_week_score(uid,nickname,distance,score,total_score,guai) 
+VALUES( 1,'22',11,11,11,11)
+
+##定时器每周清理上周积分(周一0点执行)
+TRUNCATE TABLE cur_week_score;
+
+
 ##取实时排行榜
 ##设入参num,num为每次取的记录数
 ##设入参temp_id，temp_id为每次取的开始名次数(最小为1)
-SELECT @a:=IFNULL(@a,temp_id-1)+1 AS rankid,  n.uid,a.nickname,n.oneDistance,n.oneSocre,n.oneAllsocre,n.oneGuai
-FROM
-(
-SELECT MAX(id) AS id,p.uid,p.oneAllsocre
-FROM
-(SELECT uid,MAX(oneAllsocre) AS oneAllsocre
-FROM `one_game_log`
-WHERE  posttime BETWEEN SUBDATE(CURDATE(),IF(DAYOFWEEK(CURDATE())=1,6,DATE_FORMAT(CURDATE(),'%w')-1)) AND NOW()
-GROUP BY uid) p
-JOIN `one_game_log` l ON l.uid=p.uid AND l.oneAllsocre= p.oneAllsocre
-GROUP BY p.uid,p.oneAllsocre
-) s
-JOIN `accountusers` a ON s.uid = a.uid
-JOIN `one_game_log` n ON n.id = s.id
-ORDER BY n.oneAllsocre DESC
+SELECT @a:=IFNULL(@a,temp_id-1)+1 AS rankid,`uid` ,`nickname` ,`distance` ,`score` ,`total_score` ,`guai` 
+FROM cur_week_score s JOIN (SELECT @a:=0) a
+ORDER BY s.total_score DESC
 LIMIT temp_id,num;
 
-##查询用户当前的排名(未取详细信息)
+##查询用户当前的排名
 ##设入参i_uid为所取用户标志 
-SELECT k.rankid
+SELECT rankid,`uid` ,`nickname` ,`distance` ,`score` ,`total_score` ,`guai` 
 FROM (
-SELECT @a:=IFNULL(@a,0)+1 AS rankid,n.uid
-FROM
-(
-SELECT MAX(id) AS id,p.uid,p.oneAllsocre
-FROM
-(SELECT uid,MAX(oneAllsocre) AS oneAllsocre
-FROM `one_game_log`
-WHERE  posttime BETWEEN SUBDATE(CURDATE(),IF(DAYOFWEEK(CURDATE())=1,6,DATE_FORMAT(CURDATE(),'%w')-1)) AND NOW()
-GROUP BY uid) p
-JOIN `one_game_log` l ON l.uid=p.uid AND l.oneAllsocre= p.oneAllsocre
-GROUP BY p.uid,p.oneAllsocre
-) s
-JOIN `one_game_log` n ON n.id = s.id
-ORDER BY n.oneAllsocre DESC
-LIMIT 1,1000000) k
-WHERE k.uid=i_uid
+SELECT @a:=IFNULL(@a,0)+1 AS rankid,`uid` ,`nickname` ,`distance` ,`score` ,`total_score` ,`guai` 
+FROM cur_week_score s JOIN (SELECT @a:=0) a
+ORDER BY s.`total_score` DESC) k
+WHERE uid=i_uid
+
 ##通过传入用户的排名，获取前后10名
 ##设入参i_rank为所取用户当前排名
-SELECT r.rankid,r.uid,r.nickname,r.oneDistance,r.oneSocre,r.oneAllsocre,r.oneGuai
+SELECT  rankid,`uid` ,`nickname` ,`distance` ,`score` ,`total_score` ,`guai` 
 FROM 
 (
-SELECT @a:=IFNULL(@a,0)+1 AS rankid,n.uid,a.nickname,n.oneDistance,n.oneSocre,n.oneAllsocre,n.oneGuai
-FROM
-(
-SELECT MAX(id) AS id,p.uid,p.oneAllsocre
-FROM
-(SELECT uid,MAX(oneAllsocre) AS oneAllsocre
-FROM `one_game_log`
-WHERE  posttime BETWEEN SUBDATE(CURDATE(),IF(DAYOFWEEK(CURDATE())=1,6,DATE_FORMAT(CURDATE(),'%w')-1)) AND NOW()
-GROUP BY uid) p
-JOIN `one_game_log` l ON l.uid=p.uid AND l.oneAllsocre= p.oneAllsocre
-GROUP BY p.uid,p.oneAllsocre
-) s
-JOIN `accountusers` a ON s.uid = a.uid
-JOIN `one_game_log` n ON n.id = s.id
-ORDER BY n.oneAllsocre DESC
-LIMIT 1,1000000) r
+SELECT @a:=IFNULL(@a,0)+1 AS rankid,`uid` ,`nickname` ,`distance` ,`score` ,`total_score` ,`guai` 
+FROM cur_week_score s JOIN (SELECT @a:=0) a
+ORDER BY s.total_score DESC) r
 WHERE r.rankid BETWEEN (i_rank-10) AND (i_rank+10)
-
